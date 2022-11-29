d1ad0cff7729807aff021b63c03eb19380553b96
MathiTarazonad <vladitarazona@gmail.com>
2022-11-29 12:00:20 -0300

    Added function to handle temperature change and a classname for the one that is selected

--- src/components/OptionComponents/Temperatures.jsx
+++ src/components/OptionComponents/Temperatures.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setIfCelsius } from '../../store/temperatureSlice';
 
 export default function Temperatures() {
+  const dispatch = useDispatch();
+  const isCelsius = useSelector((store) => store.temperature.isCelsius);
+
+  const handleTemperature = (temp) => {
+    if (temp === 'C' && !isCelsius) {
+      dispatch(setIfCelsius());
+    } else if (temp === 'F' && isCelsius) {
+      dispatch(setIfCelsius());
+    }
+  };
+
   return (
-    <div className='temperatures-container'>
-      <button className='temperature' id='celsius'>
+    <div className="temperatures-container">
+      <button
+        className={isCelsius ? 'temperature selected' : 'temperature'}
+        id="celsius"
+        onClick={() => handleTemperature('C')}
+      >
         °C
       </button>
-      <button className='temperature' id='fahrenheit'>
+      <button
+        className={!isCelsius ? 'temperature selected' : 'temperature'}
+        id="fahrenheit"
+        onClick={() => handleTemperature('F')}
+      >
         °F
       </button>
     </div>
-  )
+  );
 }
