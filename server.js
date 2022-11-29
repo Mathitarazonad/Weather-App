0e84ce1dbc351402d33da2c90cdc79ce6c06f0f5
MathiTarazonad <vladitarazona@gmail.com>
2022-11-29 11:46:03 -0300

    Created initial server with some endpoints

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const PORT = 8000;
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-require('dotenv').config();
+import express from 'express';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-const app = express();
+const app = express()
 
-app.listen(PORT,() => (console.log(`Server is running on ${PORT}`)));
+app.get('/coords', (req, res) => {
+  axios.get('https://api.api-ninjas.com/v1/geocoding?city=new york', {
+    headers : {
+      'X-Api-Key' : process.env.API_NINJA_KEY
+    }
+  }).then(resp => res.json(resp.data))
+})
 
+app.get('/country', (req, res) => {
+  const [latitute, longitude] = [req.query.lat, req.query.lon];
+  axios.get(`https://api.api-ninjas.com/v1/reversegeocoding?lat=${latitute}&lon=${longitude}`, 
+  {
+    headers : {
+      'X-Api-Key' : process.env.API_NINJA_KEY
+    }
+  }
+  ).then(resp => res.json(resp.data))
+})
+
+app.listen(8000, () => {
+  console.log('Servidor escuchando en puerto 8000');
+})
