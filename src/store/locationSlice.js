39eb639102a92c517dd15bb1c686154d177b5701
MathiTarazonad <vladitarazona@gmail.com>
2022-12-05 16:31:44 -0300

    Added current time state and its reducer

--- src/store/locationSlice.js
+++ src/store/locationSlice.js
@@ -6,10 +6,16 @@ const initialState = {
     city : 'New York',
     country : 'us',
     coords: {
-      latitude : 40.710335,
-      longitude : -73.99307
+      latitude : 40.7127281,
+      longitude : -74.0060152
     },
-    timezone: 'America/New_York'
+    timezone: 'America/New_York',
+    time: {
+      hour: '',
+      day: '',
+      dayName: '',
+      month: '',
+    }
  }
 }
 
@@ -26,15 +32,22 @@ const locationSlice = createSlice({
       state.current.country = country;
       state.current.timezone = timezone;
       state.current.coords.latitude = latitude;
-      state.current.coords.longitude = longitude
+      state.current.coords.longitude = longitude;
     },
     setCurrentCoords: (state,action) => {
       const {latitude, longitude} = action.payload
       state.current.coords.latitude = latitude,
       state.current.coords.longitude = longitude
+    },
+    setCurrentTime: (state, action) => {
+      const {hour, day, dayName, month} = action.payload;
+      state.current.time.day = day;
+      state.current.time.dayName = dayName;
+      state.current.time.month = month;
+      state.current.time.hour = hour;
     }
   }
 });
 
 export default locationSlice.reducer;
-export const {setSearchedCity, setCurrentLocationInfo, setCurrentCoords} = locationSlice.actions;
+export const {setSearchedCity, setCurrentLocationInfo, setCurrentCoords, setCurrentTime} = locationSlice.actions;
