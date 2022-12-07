1a25128df73aff281b7c2dd7d0d65f317b6fc4e8
MathiTarazonad <vladitarazona@gmail.com>
2022-12-07 11:16:44 -0300

    Changed component to render daily weather with real data

--- src/components/WeeklyComponents/WeeklyWeather.jsx
+++ src/components/WeeklyComponents/WeeklyWeather.jsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import DailyWeather from './DailyWeather'
+import DailyWeather from './DailyWeather';
+import { useGetTodayWeatherQuery } from '../../apis/weatherApi';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getArrayOfDays } from '../functions';
+import { setDailyWeather } from '../../store/weatherSlice';
 
 export default function WeeklyWeather() {
+  const dispatch = useDispatch();
+  const { latitude, longitude } = useSelector((store) => store.locations.current.coords);
+  const { timezone } = useSelector((store) => store.locations.current);
+  const { isCelsius } = useSelector((store) => store.temperature);
+  const { dailyWeather } = useSelector((store) => store.weather);
+  const { data: dailyWeatherData, isSuccess } = useGetTodayWeatherQuery({latitude, longitude, timezone, isCelsius,});
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setDailyWeather(getArrayOfDays(dailyWeatherData)));
+    }
+  }, [dailyWeatherData]);
+
   return (
-    <div className='weather-list-container'>
-      <DailyWeather />
-      <DailyWeather />
-      <DailyWeather />
-      <DailyWeather />
-      <DailyWeather />
+    <div className="weather-list-container">
+      {dailyWeather.map((singleDay) => (
+        <DailyWeather
+          day={singleDay.day}
+          dayName={singleDay.dayName}
+          month={singleDay.month}
+          minTemp={singleDay.minTemp}
+          maxTemp={singleDay.maxTemp}
+          weatherCode={singleDay.weatherCode}
+        />
+      ))}
     </div>
-  )
+  );
 }
