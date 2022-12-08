6c70ad5d4eb9ea2195357134f469f4a771f4ee86
MathiTarazonad <vladitarazona@gmail.com>
2022-12-08 14:43:31 -0300

    Edited hook to get location by cities and deleted countrycode argument, also exported the hook

--- src/apis/locationsApi.js
+++ src/apis/locationsApi.js
@@ -5,7 +5,7 @@ export const locationsAPI = createApi({
   baseQuery : fetchBaseQuery({baseUrl: 'http://localhost:8000'}),
   endpoints : builder => ({
     getLocationByCity : builder.query({
-      query : ({city, country}) => `/city?city=${city}&country=${country}`
+      query : (city) => `/city?city=${city}`
     }),
     getLocationByCoords : builder.query({
       query : ({latitude, longitude}) => `/coords?latitude=${latitude}&longitude=${longitude}`
@@ -13,4 +13,4 @@ export const locationsAPI = createApi({
   })
 });
 
-export const { useGetCurrentLocationQuery, useGetLocationByCoordsQuery } = locationsAPI;
+export const { useGetLocationByCityQuery, useGetCurrentLocationQuery, useGetLocationByCoordsQuery } = locationsAPI;
