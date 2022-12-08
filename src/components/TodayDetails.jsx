55931ab14c51e96a1f7f44b93353a60f0a58193a
MathiTarazonad <vladitarazona@gmail.com>
2022-12-08 08:45:51 -0300

    Changed component to show real time information

--- src/components/TodayDetails.jsx
+++ src/components/TodayDetails.jsx
@@ -1,20 +1,46 @@
-import React from 'react';
-import { FaLocationArrow } from 'react-icons/fa'
+import { FaLocationArrow } from 'react-icons/fa';
+import { useGetTodaysDetailsQuery } from '../apis/weatherApi';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setTodayDetails } from '../store/weatherSlice';
+import { getCompassDirection, getTemperatureFormat, roundTemperature } from './functions';
 
 export default function () {
-  return (
+  const dispatch = useDispatch();
+  const {latitude, longitude} = useSelector(store => store.locations.current.coords);
+  const {timezone} = useSelector(store => store.locations.current);
+  const {isCelsius} = useSelector(store => store.temperature);
+  const {humidity, visibility, windSpeed, windDirection, apparentTemperature} = useSelector(store => store.weather.todayWeather.details);
+  const {data: todayDetailsData, isSuccess} = useGetTodaysDetailsQuery({latitude, longitude, timezone, isCelsius});
+
+  useEffect(() => {
+    if (isSuccess) {
+      const currentHourIndex = todayDetailsData.hourly.time.indexOf(todayDetailsData.current_weather.time);
+      dispatch(setTodayDetails({
+        humidity: todayDetailsData.hourly.relativehumidity_2m[currentHourIndex],
+        visibility: todayDetailsData.hourly.visibility[currentHourIndex]/1000,
+        windSpeed: todayDetailsData.current_weather.windspeed,
+        windDirection: todayDetailsData.current_weather.winddirection,
+        apparentTemperature: todayDetailsData.hourly.apparent_temperature[currentHourIndex]
+      }))
+    }
+  }, [todayDetailsData]);
+
+  if (isSuccess) {
+    return (
     <div className='today-details'>
-      <div className='today-wind'>
+      <h2>Current Weather Details</h2>
+      <div className='current-wind'>
         <h3 className='details-titles'>Wind Status</h3>
-        <p><b className='current-detail'>7</b>mph</p>
+        <p><b className='current-detail'>{(windSpeed)}</b>km/h</p>
         <div className='wind-direction'>
           <FaLocationArrow className='wind-icon'/>
-          <p>WSW</p>
+          <p>{getCompassDirection(windDirection)}</p>
         </div>
       </div>
-      <div className='today-humidity'>
+      <div className='current-humidity'>
         <h3 className='details-titles'>Humidity</h3>
-        <p className='current-detail'>84%</p>
+        <p className='current-detail'>{humidity}%</p>
         <div className='progress-bar-container'>
           <div className='percentage-points'>
             <p>0</p>
@@ -25,14 +51,15 @@ export default function () {
           <p>%</p>
         </div>
       </div>
-      <div className='today-visibility'>
+      <div className='current-visibility'>
         <h3 className='details-titles'>Visibility</h3>
-        <p className='current-detail'>6,4</p><p>miles</p>
+        <p className='current-detail'>{visibility >= 1 ? Math.round(visibility) : (visibility)}</p><p>km</p>
       </div>
-      <div className='today-air-pressure'>
-        <h3 className='details-titles'>Air Pressure</h3>
-        <p className='current-detail'>998</p><p>mb</p>
+      <div className='current-apparent-temperature'>
+        <h3 className='details-titles'>Apparent Temperature</h3>
+        <p className='current-detail'>{roundTemperature(apparentTemperature)}°</p><p>{getTemperatureFormat(isCelsius)}</p>
       </div>
     </div>
-  )
+    )
+  }
 }
