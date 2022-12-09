f1764b65a5b26be147453e9fe3af23d62555157f
MathiTarazonad <vladitarazona@gmail.com>
2022-12-09 09:12:17 -0300

    Created component to render cities options when user searchs with info about each one

---
+++ src/components/SidebarComponents/LocationsOptionsSearching.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function LocationsOptionsSearching({isLoading}) {
+  const cities = useSelector(store => store.locations.citiesOptions);
+  return (
+    <div className='locations-options-searching'>
+      {isLoading ? <p>Searching for cities...</p> : cities.map(city => 
+      <div className='searching-option'>
+        <div className='-info'>
+          <h3>{city.city}</h3>
+          <p>{city.state}</p>
+        </div>
+        <div className='-flag'>
+          <img src={`https://flagcdn.com/48x36/${city.country_code}.png`} alt={city.country+'Flag'} />
+        </div>
+      </div>)}
+    </div>
+  )
+}
