34d683d6154bd107830c6454b975fedc84995b21
MathiTarazonad <vladitarazona@gmail.com>
2022-12-09 09:34:02 -0300

    Added keys where necessary

--- src/components/SidebarComponents/LocationsOptionsSearching.jsx
+++ src/components/SidebarComponents/LocationsOptionsSearching.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import {v4 as uuid} from 'uuid';
+import { useSelector } from 'react-redux';
 
 export default function LocationsOptionsSearching({isLoading}) {
   const cities = useSelector(store => store.locations.citiesOptions);
   return (
     <div className='locations-options-searching'>
       {isLoading ? <p>Searching for cities...</p> : cities.map(city => 
-      <div className='searching-option'>
+      <div className='searching-option' key={uuid()}>
         <div className='-info'>
           <h3>{city.city}</h3>
           <p>{city.state}</p>
