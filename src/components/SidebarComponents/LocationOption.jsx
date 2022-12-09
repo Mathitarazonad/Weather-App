42cf77ff01e20c8d9745f2efa2aa6addc45f6c73
MathiTarazonad <vladitarazona@gmail.com>
2022-12-09 09:32:09 -0300

    Fixed component to show real time information, city instead of location and added functionality to change the current location when user clicks on a city previously searched or that is saved

--- src/components/SidebarComponents/LocationOption.jsx
+++ src/components/SidebarComponents/LocationOption.jsx
@@ -1,14 +1,32 @@
 import { AiFillCaretRight, AiOutlineCloseCircle } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentLocationInfo } from '../../store/locationSlice';
 
-export default function LocationOption({location}) {
+export default function LocationOption({city}) {
+  const dispatch = useDispatch();
+  const {city: currentCity, stateName} = useSelector(store => store.locations.current);
+  
+  const handleClick = () => {
+    if(currentCity !== city.city && stateName !== city.stateName) {
+      dispatch(setCurrentLocationInfo({
+        city : city.city,
+        stateName : city.stateName,
+        country : city.country,
+        timezone: city.timezone,
+        latitude: city.latitude,
+        longitude: city.longitude
+      }));
+    }
+  };
+
   return (
     <div className='location-option'>
       <div className='location-info'>
-        <div className='location-info-text'>
-          <p>{location.city}</p>
-          <p>{location.state}</p>
+        <div className='location-info-text' onClick={() => handleClick()}>
+          <p>{city.city}</p>
+          <p>State - {city.stateName}</p>
         </div>
-        <img src={`https://flagcdn.com/48x36/${location.country_code}.png`} className='location-flag'/>
+        <img src={`https://flagcdn.com/48x36/${city.country}.png`} className='location-flag'/>
       </div>
       <div className='location-btns-container'>
         <button className='location-btn' id='location-select-btn'><AiFillCaretRight /></button>
