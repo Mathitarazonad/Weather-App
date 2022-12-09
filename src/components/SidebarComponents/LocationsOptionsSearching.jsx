9f2bd38b3acde40475ce3e08db78c38373c1d2c7
MathiTarazonad <vladitarazona@gmail.com>
2022-12-09 11:05:31 -0300

    Added functionality to set current city when user click in one option, and only if city is not already saved

--- src/components/SidebarComponents/LocationsOptionsSearching.jsx
+++ src/components/SidebarComponents/LocationsOptionsSearching.jsx
@@ -1,12 +1,28 @@
 import {v4 as uuid} from 'uuid';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { setCurrentLocationInfo, setSearchedCitiesOptions, setIfSearchingCities, setSearchedCity, setCitiesOptions } from '../../store/locationSlice.js';
+import { citiesAreNotEqual } from '../functions.js'
 
 export default function LocationsOptionsSearching({isLoading}) {
+  const dispatch = useDispatch();
   const cities = useSelector(store => store.locations.citiesOptions);
+  const citiesSaved = useSelector(store => store.locations.searchedCitiesOptions)
+
+  const handleClick = cityOption => {
+    const {city, country_code, lat, lon, timezone, state} = cityOption;
+    if (citiesAreNotEqual(citiesSaved, city, state)) {
+      dispatch(setCurrentLocationInfo({city, country: country_code, latitude: lat, longitude: lon, timezone : timezone.name, stateName : state}));
+      dispatch(setSearchedCitiesOptions({city, country: country_code, latitude: lat, longitude: lon, timezone : timezone.name, stateName : state}));
+      dispatch(setIfSearchingCities(false));
+      dispatch(setSearchedCity(''));
+      dispatch(setCitiesOptions([]))
+    }
+  }
+
   return (
     <div className='locations-options-searching'>
       {isLoading ? <p>Searching for cities...</p> : cities.map(city => 
-      <div className='searching-option' key={uuid()}>
+      <div className='searching-option' key={uuid()} onClick={() => handleClick(city)}>
         <div className='-info'>
           <h3>{city.city}</h3>
           <p>{city.state}</p>
