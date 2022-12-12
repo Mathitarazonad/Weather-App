95213fd277b57d63afec43ca0c1e562fd17f1bf0
MathiTarazonad <vladitarazona@gmail.com>
2022-12-12 09:10:58 -0300

    Added icon to the theme btn component

--- src/components/OptionComponents/ThemeBtn.jsx
+++ src/components/OptionComponents/ThemeBtn.jsx
@@ -1,7 +1,9 @@
+import { FaRegMoon } from 'react-icons/bs'
+
 export default function ThemeBtn() {
   return (
     <div className='theme-btn-contatiner'>
-      
+      <FaRegMoon className='theme-icon' />
     </div>
   )
 }
