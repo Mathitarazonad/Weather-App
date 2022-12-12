0fddf25995c5dadb0260c3c68219fe9f3700c2d4
MathiTarazonad <vladitarazona@gmail.com>
2022-12-12 09:25:53 -0300

    Fixed wrong route of icon and added functionality to change the theme

--- src/components/OptionComponents/ThemeBtn.jsx
+++ src/components/OptionComponents/ThemeBtn.jsx
@@ -1,8 +1,17 @@
-import { FaRegMoon } from 'react-icons/bs'
+import { FaRegMoon } from 'react-icons/fa';
+import { useDispatch } from 'react-redux';
+import { setIfDarkMode } from '../../store/themeSlice';
 
 export default function ThemeBtn() {
+
+  const dispatch = useDispatch();
+
+  const handleThemeChange = () => {
+    dispatch(setIfDarkMode());
+  }
+
   return (
-    <div className='theme-btn-contatiner'>
+    <div className='theme-btn-container' onClick={() => handleThemeChange()}>
       <FaRegMoon className='theme-icon' />
     </div>
   )
