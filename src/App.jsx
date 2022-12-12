2fa650cc205f8a81f4ce082aa6ac92db4316867f
MathiTarazonad <vladitarazona@gmail.com>
2022-12-12 09:27:22 -0300

    Added all components needed, className for lightMode if necessary and to show the week or hourly weather based on state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import { useSelector } from 'react-redux';
+import HourlyWeather from './components/HourlyComponents/HourlyWeather';
+import Buttons from './components/OptionComponents/Buttons';
 import Temperatures from './components/OptionComponents/Temperatures';
-import Sidebar from './components/Sidebar';
+import WeatherOptions from './components/OptionComponents/WeatherOptions';
+import Sidebar from './components/SidebarComponents/Sidebar';
+import TodayDetails from './components/TodayComponents/TodayDetails';
+import WeekWeather from './components/WeeklyComponents/WeekWeather';
+import './sass/main.css';
 
 function App() {
+
+  const { dailyWeatherMode } = useSelector(store => store.weather);
+  const { isDarkMode } = useSelector(store => store.theme);
+
   return (
-    <div className="App">
-      <Sidebar />
-      <Temperatures />
+    <div className={isDarkMode ? 'App' : 'App light-mode'}>
+      <main className='main-container'>
+        <Sidebar />
+        <section id='weather-section'>
+          <Buttons />
+          <WeatherOptions />
+          {dailyWeatherMode ? <WeekWeather /> : <HourlyWeather />}
+          <TodayDetails />
+        </section>
+      </main>
+      <footer>Created by <a href='#'><b>MathiTarazonad</b></a> - Challenge from <a href='#'><b>devChallenges.io</b></a></footer>
     </div>
   );
 }
