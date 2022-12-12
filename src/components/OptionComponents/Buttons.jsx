e21a7c1d33f08f07c3c2114158f6524eb7bd9c43
MathiTarazonad <vladitarazona@gmail.com>
2022-12-12 09:06:59 -0300

    Created component to render the buttons of temperature and theme changers

---
+++ src/components/OptionComponents/Buttons.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Temperatures from './Temperatures'
+
+export default function Buttons() {
+  return (
+    <section id='buttons-section'>
+      <Temperatures />
+    </section>
+  )
+}
