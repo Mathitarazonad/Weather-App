2ff74da4b9769e7e6ce7e9079177342a3522b4f5
MathiTarazonad <vladitarazona@gmail.com>
2022-12-14 15:41:32 -0300

    Added className when configurations are open and other icon  for configurations, also implemented functionalities for close and open the configurations

--- src/components/OptionComponents/Buttons.jsx
+++ src/components/OptionComponents/Buttons.jsx
@@ -1,12 +1,39 @@
-import Temperatures from './Temperatures'
-import ThemeBtn from './ThemeBtn'
+import Temperatures from './Temperatures';
+import ThemeBtn from './ThemeBtn';
+import { useSelector, useDispatch } from 'react-redux';
+import { VscSettingsGear } from 'react-icons/vsc';
+import { AiOutlineClose } from 'react-icons/ai';
+import { setIfConfigurationState } from '../../store/menuSlice';
 
 export default function Buttons() {
+  const dispatch = useDispatch();
+  const { configurationsAreOpen } = useSelector((store) => store.menu);
+
+  const handleConfigurations = () => {
+    dispatch(setIfConfigurationState());
+  };
 
   return (
-    <section id='buttons-section'>
+    <section
+      className={
+        configurationsAreOpen
+          ? 'buttons-section configurations-open'
+          : 'buttons-section'
+      }
+    >
       <Temperatures />
       <ThemeBtn />
+      {!configurationsAreOpen ? (
+        <VscSettingsGear
+          className="configurations-icon"
+          onClick={() => handleConfigurations()}
+        />
+      ) : (
+        <AiOutlineClose
+          className="configurations-close-icon"
+          onClick={() => handleConfigurations()}
+        />
+      )}
     </section>
-  )
+  );
 }
