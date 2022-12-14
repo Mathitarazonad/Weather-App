2b6d64dcc92a7005c7a7ae2babe279eb3d9036cc
MathiTarazonad <vladitarazona@gmail.com>
2022-12-14 15:37:12 -0300

    Added configurations state when it's open or not and its reducer

--- src/store/menuSlice.js
+++ src/store/menuSlice.js
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   menuIsOpen : false,
+  configurationsAreOpen : false,
 }
 
 const menuSlice = createSlice({
@@ -10,9 +11,12 @@ const menuSlice = createSlice({
   reducers : {
     setIfMenuOpen : state => {
       state.menuIsOpen = !state.menuIsOpen;
+    },
+    setIfConfigurationState : state => {
+      state.configurationsAreOpen = !state.configurationsAreOpen
     }
   }
 })
 
 export default menuSlice.reducer;
-export const { setIfMenuOpen } = menuSlice.actions;
+export const { setIfMenuOpen, setIfConfigurationState } = menuSlice.actions;
