b19c3f847683b117f85a8d0681d0ff981b6d7159
MathiTarazonad <vladitarazona@gmail.com>
2022-12-29 14:12:17 -0300

    Changed base url to fetch

--- client/src/apis/locationsApi.js
+++ client/src/apis/locationsApi.js
@@ -2,7 +2,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 export const locationsAPI = createApi({
   reducerPath : 'locationsAPI',
-  baseQuery : fetchBaseQuery({baseUrl: 'https://weather-app-fikm.onrender.com'}),
+  baseQuery : fetchBaseQuery({baseUrl: 'https://weather-appmt-server.vercel.app'}),
   endpoints : builder => ({
     getLocationByCity : builder.query({
       query : (city) => `/city?city=${city}`
